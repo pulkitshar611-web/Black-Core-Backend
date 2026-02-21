09225f5eac57f61bb93774e9ade964e2ed87dca3
lightlabcreation <lightlabcreation@gmail.com.com>
2026-02-21 17:41:06 +0530

    rohit

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const prisma = new PrismaClient();
 // ─── Socket.io Setup ─────────────────────────────────────────
 const io = new Server(server, {
     cors: {
-        origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+        origin: process.env.FRONTEND_URL || 'http://localhost:5173' || "https://blackcore.kiaantechnology.com",
         methods: ['GET', 'POST'],
     },
 });
